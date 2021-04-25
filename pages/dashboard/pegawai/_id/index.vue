1c213dfbeac0c59cffa86d225ea91bda424e8505
Vriyas Hartama <vriyashartama@gmail.com>
2021-04-25 21:31:38 +0700

    Fix unscoped button on delete and edit

--- pages/dashboard/pegawai/_id/index.vue
+++ pages/dashboard/pegawai/_id/index.vue
@@ -17,6 +17,7 @@
         </div>
         <div class="flex my-auto">
           <button
+            v-if="$auth.hasScope('employee.update')"
             class="text-gray-500 inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm
             font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2
             focus:ring-offset-2 focus:ring-gray-500"
@@ -25,7 +26,7 @@
             Edit
           </button>
           <button
-            v-if="employee.data.locked !== 1"
+            v-if="employee.data.locked !== 1 && $auth.hasScope('employee.update')"
             class="ml-2 text-gray-500 inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm
             font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2
             focus:ring-offset-2 focus:ring-gray-500"
@@ -39,7 +40,7 @@
             </span>
           </button>
           <button
-            v-if="employee.data.locked !== 1"
+            v-if="employee.data.locked !== 1 && $auth.hasScope('employee.delete')"
             type="submit"
             class="ml-2 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm
             text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none
